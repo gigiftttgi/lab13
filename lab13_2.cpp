fb39e82ffcfe6481eacf35e8a313b11256a60d1a
gigiftttgi <natcha_si@cmu.ac.th>
2021-01-22 17:37:40 +0700

    lab13_2_final

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -19,3 +19,46 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double mat[][N]){
+	for(int i=0;i<N;i++)
+	{
+		cout << "Row " << i+1 << ": ";
+		for(int j=0;j<N;j++)
+		{
+		cin >> mat[i][j];
+		}
+	}
+	}
+	
+void findLocalMax(const double mat[][N], bool show[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			if(i==0 or i==N-1){
+				show[i][j] = false;
+			}
+			else if(j==0 or j==N-1)
+			{
+				show[i][j] = false;
+			}
+			else
+			{
+				if(mat[i][j]>=mat[i-1][j] and mat[i][j]>=mat[i][j-1] and mat[i][j]>=mat[i][j+1] and mat[i][j]>=mat[i+1][j])
+					show[i][j] = true;
+				else
+					show[i][j] = false;
+			}		
+		}
+	}	
+}
+	
+
+void showMatrix(const bool mat[][N])
+{
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			cout << mat[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
